76d44630c15c0cbcee8190591f24ff237ec02b93
Ashish Sharma <ashishsharma@Ashishs-MacBook-Pro.local>
2019-05-13 00:03:09 +0530

    added config for dev and production

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const app = express();
 import api from './api/api';
+import config from './config/index';
+
+console.log(config);
 // require('mongoose').connect(`mongodb://localhost/nodeblog`);
-require('mongoose').connect(`mongodb://admin:admin123@ds261155.mlab.com:61155/nodeblog`);
+require('mongoose').connect(`${config.dbUrl}`);
 
 require('./middleware/appMiddleware')(app);
 
