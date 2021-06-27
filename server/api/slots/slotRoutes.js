5f1e2329b7e14c8d2e6a2a4f328ab32b6c90b307
Ashishs1390 <ashishhsharma1990@gmail.com>
2021-06-28 00:27:04 +0530

    update slots

--- server/api/slots/slotRoutes.js
+++ server/api/slots/slotRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-import {post,slotBook} from './slotsController'
+import {post,slotBook,getSlot} from './slotsController'
 router.route('/').post(post);
 router.route('/bookslot').post(slotBook);
+router.route('/').get(getSlot);
 
 
 module.exports = router;
--- server/api/slots/slotsController.js
+++ server/api/slots/slotsController.js
@@ -88,27 +88,48 @@ export function post(req,res,next){
 }
 
 export function slotBook(req,res,next){
-    const {date,startTime,duration} = req.body;
+    const {date,startTime,duration,timeZone} = req.body;
     Slots.find({date}).then((result)=>{
         console.log(result);
         let openSlot = result[0].timeSlots.filter((val)=> val.startTime == startTime);
         let otherSlots = result[0].timeSlots.filter((val)=> val.startTime !== startTime);
 
-        console.log(openSlot);
         openSlot[0].booked = true;
         console.log(otherSlots);
         otherSlots.push(...openSlot);
+        let updatedArr = {
+            date,
+            timeZone,
+            timeSlots:otherSlots
+        }
+        Slots.updateOne({date},updatedArr).then((result)=>{
+            console.log(result);
+            res.send(otherSlots);
 
+        }).catch((err)=>{
+            console.log(err)
+        })
 
-        res.send(otherSlots);
     }).catch((err)=>{
         console.log(err);
-    })
-
-
-
+        res.send(err);
+    });
 }
 
+export function getSlot(req,res,next){
+    const {startTime,endTime,date} = req.body;
+    Slots.find({date}).then((result)=>{
+        let slots = result[0].timeSlots;
+        let startDataIndex = slots.findIndex(obj => obj.startTime== startTime);
+        let endDataIndex = slots.findIndex(obj => obj.endTime== endTime);
+        let newSlots = slots.splice(startDataIndex,endDataIndex);
+        res.send(newSlots);
+    }).catch(err=>{
+        console.log(err);
+        res.send(err);
+    });
+
+}
 
 
 
